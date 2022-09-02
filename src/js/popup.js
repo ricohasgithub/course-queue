6572d28a171bc1fd52305ad823cf079832a1353a
Rico Zhu <rico.zhu@duke.edu>
2022-09-02 16:07:55 -0400

    Updated Document Edits

--- src/js/popup.js
+++ src/js/popup.js
@@ -10,22 +10,18 @@ function loadCourses(json) {
     console.log(courses_json);
 
     for (let course in courses_json) {
-        console.log(course);
-        course_names.push(course);
-        course_links.push(courses_json[course]);
-    }
 
-    // Create new html elements in popup.html for each link
-    let i = 0;
-    for (let course in course_names) {
-        console.log(course);
-        for (let course_index in course_links[i]) {
-            const para = document.createElement("p");
-            const node = document.createTextNode(course_links[i][course_index]);
-            para.appendChild(node);
-            console.log(course_links[i][course_index]);
-            i++;
-        }
+        // Create new html elements in popup.html for each link
+
+        course_names.push(course);
+
+        const para = document.createElement("p");
+        const node = document.createTextNode(course);
+        para.appendChild(node);
+        document.getElementById("courses").innerHTML = para;
+
+        course_links.push(courses_json[course]);
+
     }
 
 }
