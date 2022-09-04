2e24316193601cd260f054c95dd4c494c1b515da
Rico Zhu <rico.zhu@duke.edu>
2022-09-04 13:40:09 -0400

    Added Inject Element Click Callback

--- src/js/popup.js
+++ src/js/popup.js
@@ -21,11 +21,18 @@ function loadCourses(json) {
         course_links.push(courses_json[course]);
         for (let course_index in courses_json[course]) {
             let course_url = courses_json[course][course_index];
-            document.getElementById("courses").innerHTML += "<a href='" + course_url + "'target='_blank'>" + "hello </a>";
+            document.getElementById("courses").innerHTML += `<a href=` + course_url + `'target='_blank' id = 'tabButton${course_url}' class = 'tabButton'>` + `hello </a>`;
+            document.getElementById(`tabButton${course_url}`).onclick = openTabGroup(course_url);
         }
 
     }
 
+}
+
+function openTabGroup(course_url) {
+    console.log("Hello");
+    console.log(course_url);
+    window.open(course_url, '_blank').focus();
 }
 
 let url = chrome.runtime.getURL('./assets/courses.json');
